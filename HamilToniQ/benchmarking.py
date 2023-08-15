0edd1ed37e75c736ed071e95ee944326021922d1
Felix Xu <61252303+FelixXu35@users.noreply.github.com>
2023-08-15 15:37:14 +0100

    first circuit run

--- HamilToniQ/benchmarking.py
+++ HamilToniQ/benchmarking.py
@@ -4,31 +4,39 @@ This Class is defined to give an overall score of the QAOA performance on a quan
 
 from typing import Callable, List, Any
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 
 import numpy as np
 from pathlib import Path
 from scipy.optimize import minimize
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, Aer
+from qiskit.algorithms.optimizers import COBYLA
 from functools import partial
 
-from .qiskit_tools import Ising_to_ansatz, qiskit_sampler
-from .unitity import Q_to_Ising
+from .unitity import build_cost_func, load_qiskit_sampler
 from .matrices import *
 
 Counts = Any
 Circuit = Any
 
+
 class Toniq:
     def __init__(self) -> None:
         self.Q = dim_4_var_8
         self.rep = 3
+        self.n_layers = 2
         pass
 
-    def run(self, ansatz: Callable[[list, list, int], Circuit], sampler: Callable[[Circuit], Counts]) -> float:
-        paulis_terms, weights, offset = Q_to_Ising(self.Q)
-        loaded_ansatz = partial(ansatz, paulis_terms, weights, self.rep)
-
-        counts = sampler(loaded_ansatz)
-        cost = self.counts_to_cost(counts, Q)
-        
+    def run(
+        self,
+        #ansatz: Callable[[list, list, int], Circuit],
+        #sampler: Callable[[Circuit], Counts],
+    ) -> float:
+        backend = Aer.get_backend('statevector_simulator')
+        sampler = load_qiskit_sampler(backend, 1000)
+        cost_func = build_cost_func(sampler, self.Q, self.n_layers)
+        optimizer = COBYLA(maxiter=1000)
+        init_params = [2 for i in range(2*self.n_layers)]
+        result = optimizer.minimize(fun=cost_func, x0=init_params)
+        return result.x
--- HamilToniQ/unitity.py
+++ HamilToniQ/unitity.py
@@ -1,11 +1,17 @@
 from typing import List, Any, Callable, Dict
 
 import numpy as np
+from functools import partial
 from qiskit import QuantumCircuit
+from qiskit.providers.ibmq import IBMQ
+from qiskit_aer import AerSimulator
+from qiskit import transpile, QuantumCircuit
+from qiskit.result.counts import Counts
 
 matrix = Any
 circuit = Any
 counts = Dict
+
 
 def return_hardness(return_vec: list) -> float:
     """
@@ -49,7 +55,7 @@ class instance_generator:
 def counts_to_cost(counts: counts, Q: matrix) -> float:
     cost = 0
     n_samples = 0
-    for key, value in counts.items:
+    for key, value in counts.items():
         vector = [int(i) for i in key]
         cost += np.dot(vector, np.dot(Q, vector)) * value
         n_samples += value
@@ -73,10 +79,33 @@ def params_to_distribution(
 
     return distribution
 
+
+def get_fake_backend(backend_name: str) -> AerSimulator:
+    # get a real backend from a real provider
+    provider = IBMQ.load_account()
+    backend = provider.get_backend("ibmq_manila")
+
+    # generate a simulator that mimics the real quantum system with the latest calibration results
+    backend_sim = AerSimulator.from_backend(backend)
+
+    return backend_sim
+
+
+def qiskit_sampler(
+    backend: AerSimulator, n_samples: int, circuit: QuantumCircuit
+) -> Counts:
+    # get the sampling results from either a simulator or a real hardware
+    transpiled_circuit = transpile(circuit, backend)
+    job = backend.run(transpiled_circuit)
+    counts = job.result().get_counts()
+
+    return counts
+
+
 def Q_to_Ising(Q):
     # input is nxn symmetric numpy array corresponding to QUBO matrix Q
 
-    n = Q.shape[0]
+    n = np.shape(Q)[0]
 
     offset = np.triu(Q, 0).sum() / 2
     pauli_terms = []
@@ -100,3 +129,44 @@ def Q_to_Ising(Q):
             weights = np.concatenate((weights, weight), axis=None)
 
     return pauli_terms, weights, offset
+
+
+def Ising_to_ansatz(pauli_terms, weights, n_layers, params):
+    n_qubits = len(pauli_terms[0])
+    circuit = QuantumCircuit(n_qubits)
+    for i in range(n_qubits):
+        circuit.h(i)
+    for j in range(n_layers):
+        for k in range(len(pauli_terms)):
+            term = pauli_terms[k]
+            index_of_ones = []
+            for l in range(len(term)):
+                if term[l] == 1:
+                    index_of_ones.append(l)
+            if len(index_of_ones) == 1:
+                circuit.rz(qubit=index_of_ones[0], phi=2 * weights[k] * params[2 * j])
+            elif len(index_of_ones) == 2:
+                circuit.rzz(
+                    qubit1=index_of_ones[0],
+                    qubit2=index_of_ones[1],
+                    theta=weights[k] * params[2 * j],
+                )
+            else:
+                raise ValueError("Invalid number of Z terms")
+
+        for i in range(n_qubits):
+            circuit.rx(theta=params[2 * j + 1], qubit=i)  # mixing terms
+    return circuit
+
+
+def cost_func(sampler, Q, n_layers, params) -> float:
+    pauli_terms, weights, offset = Q_to_Ising(Q)
+    circuit = Ising_to_ansatz(pauli_terms, weights, n_layers, params)
+    counts = sampler(circuit)
+    return counts_to_cost(counts, Q)
+
+def build_cost_func(sampler, Q, n_layers) -> Callable:
+    return partial(cost_func, sampler, Q, n_layers)
+
+def load_qiskit_sampler(backend, n_samples) -> Callable:
+    return partial(qiskit_sampler, backend, n_samples)
